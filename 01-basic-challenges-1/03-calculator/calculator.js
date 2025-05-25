619ceef5134c60aabfa920de8559ce63ffbb5940
intmain <aheleha@gmail.com>
2025-05-25 12:05:58 +0500

    01-04 done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(a, b, operation) {
+  switch (operation) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      if (b === 0) {
+        throw new Error("Division by zero");
+      }
+      return a / b;
+    default:
+      throw new Error("Invalid operation");
+  }
+}
 
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  return str.split("").filter((c) => c === char).length;
+}
+
+// function countOccurrences(str, char) {
+//   let resultStr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       resultStr.push(str[i]);
+//     }
+//   }
+//   return resultStr.length;
+// }
 
 module.exports = countOccurrences;
